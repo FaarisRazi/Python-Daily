faeea5461751a6841fafd3bbc125bc2f0c900e68
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-10 00:04:21 +0400

    Update and rename string_mods.py to modify.py
    File name updated and a new function added - mreplace!

---
+++ Strings/modify.py
@@ -0,0 +1,16 @@
+# Handy string-manipulator functions:
+
+# Multi-replace: Replace multiple sub-strings within a string
+def mreplace(text, to_replace):
+    # to_replace = dict -> {text_to_replace : new_text, ...}
+    
+    # If items-to-replace passed as a list, these items
+    # will automatically be removed from text.
+    if type(to_replace) == list: 
+        to_replace = {item:'' for item in to_replace}
+        
+    for old, new in to_replace.items(): #Replace all substrings
+        text = text.replace(old, new) #with new vals
+        
+    return text
+
