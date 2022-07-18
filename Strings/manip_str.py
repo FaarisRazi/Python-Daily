a9a5dba3abd34f3a1f4d285e6eace58fdba23dd5
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-18 06:12:12 +0400

    word_counts() function added

--- Strings/manip_str.py
+++ Strings/manip_str.py
@@ -1,11 +1,16 @@
 # Handy string-manipulator functions:
+import re
+
+#Get sub-string contained within brackets in text
+str_in_bracs = lambda s: re.search(r"\(([A-Za-z0-9_]+)\)", s)
+
 
 # Multi-replace: Replace multiple sub-strings within a string
 def mreplace(text, to_replace):
     # to_replace = dict -> {old_text : new_text, ...}
     
-    # If "to_replace" passed as a list/tuple/set, these items
-    # will be removed from the text automatically.
+    # If "to_replace" passed as a list/tuple/set, remove
+    # these items within the text automatically.
     if isinstance(to_replace, (list, tuple, set)): 
         to_replace = {item:'' for item in to_replace}
         
@@ -14,5 +19,15 @@ def mreplace(text, to_replace):
         
     return text
 
-#Get sub-string contained within brackets in text
-str_in_bracs = lambda s: re.search(r"\(([A-Za-z0-9_]+)\)", s)
+
+# Get counts of unique sub-strings/words in a string
+def word_counts(text):
+    counts = {}
+
+    for word in text.split():
+        if word in counts.keys():
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
