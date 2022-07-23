5e4fa310bc989237246f6366e67d19a5252f69e2
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-23 15:20:31 +0400

    factorial() added

--- School Kit/Maths/mymaths.py
+++ School Kit/Maths/mymaths.py
@@ -1,3 +1,8 @@
+import numpy as np
+
 # Sum numbers 1 upto x.
 #(Known as "Triangular number": https://en.wikipedia.org/wiki/Triangular_number)
 sumto = lambda x: (x**2 + x)//2
+
+# Using Numpy for factorial (instead of the commonly used recursion)
+factorial = lambda n: np.prod(np.arange(1,n+1))
