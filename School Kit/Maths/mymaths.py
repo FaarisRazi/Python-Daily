a9ff191728ad29c9fcf82784bd9a32e491beff40
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-23 15:21:52 +0400

    Added vectorized-factorial() and factorial_sums()

--- School Kit/Maths/mymaths.py
+++ School Kit/Maths/mymaths.py
@@ -6,3 +6,9 @@ sumto = lambda x: (x**2 + x)//2
 
 # Using Numpy for factorial (instead of the commonly used recursion)
 factorial = lambda n: np.prod(np.arange(1,n+1))
+
+# Above factorial() vectorized for numpy-arrays
+np_factorial = np.vectorize(factorial)
+
+# Sums of factorials upto n (1! + 2! + ... + n!)
+factorial_sums = lambda n: sum(np_factorial(np.arange(1,n+1)))
