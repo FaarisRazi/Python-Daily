3e2e8e28fa5d8cdb1db73e73778162274722d7de
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-25 18:26:53 +0400

    Update mymaths.py

--- School Kit/Maths/mymaths.py
+++ School Kit/Maths/mymaths.py
@@ -4,6 +4,9 @@ import numpy as np
 #(Known as "Triangular number": https://en.wikipedia.org/wiki/Triangular_number)
 sumto = lambda x: (x**2 + x)//2
 
+# Convert list/tuple into array of numbers
+vector = lambda lst: np.array(lst) # set/dict will be 0-dimensional arrays
+
 # Using Numpy for factorial (instead of the commonly used recursion)
 factorial = lambda n: np.prod(np.arange(1,n+1))
 
