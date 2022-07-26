0b228b34c05ac353d984f6b3d72f74ddcf6203a8
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-26 17:50:45 +0400

    Update file_manip.py

--- Files/file_manip.py
+++ Files/file_manip.py
@@ -4,6 +4,9 @@ import os
 # Get the  current working-directory
 current_dir = os.getcwd().replace('\\','/')+'/'
 
+# Check if input path is for a file. (else it is a folder)
+isfile = lambda path: os.path.isfile(path)
+
 # Get list of files/folders in directory
 files_in = lambda path: os.listdir(path)
 
