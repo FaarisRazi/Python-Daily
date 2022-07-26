57d7634042aad31598627c5aed0fcbbc4362c828
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-26 18:06:58 +0400

    istype() & remove() added, to be modified further

--- Files/file_manip.py
+++ Files/file_manip.py
@@ -7,6 +7,28 @@ current_dir = os.getcwd().replace('\\','/')+'/'
 # Check if input path is for a file. (else it is a folder)
 isfile = lambda path: os.path.isfile(path)
 
+# Check if file is of a given extension/type 
+istype = lambda file, ext='': file.endswith(ext)  
+
+def remove(path, empty=False, folders=False, types=[]):
+    if empty:
+        return os.rmdir(path)
+    
+    # If path given is a Folder/Directory
+    if not isfile(path):
+
+        # Delete all files
+        for file in files_in(path):
+
+            # Remove files with specfied extensions
+            if any(map(lambda ext: file.endswith(ext), types)):
+                os.remove(path+file)
+
+            # Remove Folders in the directory if desired
+            # elif folders and not isfile(file):
+
+        return True
+
 # Get list of files/folders in directory
 files_in = lambda path: os.listdir(path)
 
