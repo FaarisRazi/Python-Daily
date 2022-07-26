551681886798c0f2227be14615c9f31f974affae
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-26 17:10:05 +0400

    Update file_manip.py

--- Files/file_manip.py
+++ Files/file_manip.py
@@ -1,4 +1,12 @@
 # Handy functions for file/folder manipulation to be added InshaAllah! 
 import os
 
+# Get the  current working-directory
+current_dir = os.getcwd().replace('\\','/')+'/'
+
+# Get list of files/folders in directory
 files_in = lambda path: os.listdir(path)
+
+def remove(path, empty=False):
+  if empty:
+    return os.rmdir(path)
