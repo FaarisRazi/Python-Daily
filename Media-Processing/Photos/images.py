2c97737d20a5ab49290d1dddf33aecab6f0a8183
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-27 15:40:34 +0400

    remove_img_background() added (incomplete)

--- Media-Processing/Photos/images.py
+++ Media-Processing/Photos/images.py
@@ -16,6 +16,7 @@ def img_resize(photo, by_width):
     
     return photo
 
+
 # Trim Images with empty-spaces (or trim out borders in image)
 def img_trim(img):
     bg = Image.new(img.mode, img.size, img.getpixel((0,0)))
@@ -23,5 +24,29 @@ def img_trim(img):
     diff = ImageChops.add(diff, diff, 2.0, -100)
     bbox = diff.getbbox()
     if bbox:
-        return img.crop(bbox)
+        img = img.crop(bbox)
+    return img
+
+
+# Remove White/Black background from Image (to transparent)
+def remove_img_background(x, white=True, save=False):
+    remove_id = 255 if white else 0
+    
+    img = x.convert('RGB')
+    img_data = img.getdata()
+    
+    new_data = []
+    for i, item in enumerate(img_data):
+        
+        if item[0] == remove_id and item[1] == remove_id and item[2] == remove_id:
+            new_data.append(tuple([remove_id]*3+[0]))
+        else:
+            new_data.append(item)
+    
+    img.putdata(new_data)
+#     img = Image.fromarray(np.array(new_data))
+    
+    if save: 
+        img.save('img.png' if isinstance(save, bool) else f'{save}.png')
+    
     return img
