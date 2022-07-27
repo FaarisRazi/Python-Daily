4a80ae98647fdc84d0977ef92874691c22a3c4f7
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-27 15:35:36 +0400

    img_resize() added to images.py

--- Media-Processing/Photos/images.py
+++ Media-Processing/Photos/images.py
@@ -1,1 +1,17 @@
 # Handy image-processing functions to come InshaAllah!
+from PIL import Image, ImageOps
+import cv2 as cv
+import numpy as np
+
+# Invert colors in Image: 
+img_invert = lambda x: ImageOps.invert(x.convert('RGB'))
+
+# Resize an Image (automatically with given width)
+def img_resize(photo, by_width):
+    width, height = photo.size
+    wpercent = (by_width/float(width))
+    hsize = int((float(height)*float(wpercent)))
+    
+    photo = photo.resize((by_width, hsize), Image.ANTIALIAS)
+    
+    return photo
