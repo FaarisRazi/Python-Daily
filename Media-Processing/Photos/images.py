15cd3ae7cc1856d8c7d2ad9f748e76774a9969d6
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-27 15:36:41 +0400

    img_trim() added to images.py

--- Media-Processing/Photos/images.py
+++ Media-Processing/Photos/images.py
@@ -1,5 +1,5 @@
 # Handy image-processing functions to come InshaAllah!
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageChops
 import cv2 as cv
 import numpy as np
 
@@ -15,3 +15,13 @@ def img_resize(photo, by_width):
     photo = photo.resize((by_width, hsize), Image.ANTIALIAS)
     
     return photo
+
+# Trim Images with empty-spaces (or trim out borders in image)
+def img_trim(img):
+    bg = Image.new(img.mode, img.size, img.getpixel((0,0)))
+    diff = ImageChops.difference(img, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        return img.crop(bbox)
+    return img
