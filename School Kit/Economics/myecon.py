b9aac8b6b0a2413e261bb0bee5ad8067c0fd72db
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-31 21:33:31 +0400

    Quantatity Supplied/Demanded function Qsd() added.

--- School Kit/Economics/myecon.py
+++ School Kit/Economics/myecon.py
@@ -1,1 +1,4 @@
 # Handy functions for Economics to be added soon InshaAllah! 
+
+# Getting the Quantatity Supplied/Demanded 
+Qsd = lambda x,y,P: x + P*y # x = Units supplied/demanded at a price (y).
