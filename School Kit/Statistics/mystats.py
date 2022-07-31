30596f3150ef8ca54259418516e401ea3fbe5ebc
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-07-31 21:27:53 +0400

    hypergeo() distribution func added.

--- School Kit/Statistics/mystats.py
+++ School Kit/Statistics/mystats.py
@@ -1,3 +1,7 @@
 # Handy functions for Statistics to be added InshaAllah!
-# Some functions overlap with those in "Maths/mymaths.py"
 
+# (Import functions from repo-directory: Python-Daily/School Kit/Maths/mymaths.py)
+from mymaths import nCr, nPr, factorial
+
+# HyperGeometric Distribution formula
+hypergeo = lambda N, Ns, n, x: ( nCr(Ns,x) * nCr(N-Ns,n-x) ) / nCr(N,n) 
