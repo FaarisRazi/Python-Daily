0f206897db4eda2892fc6048364b0e068b7092fe
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-02 00:01:09 +0400

    Added displacement func xdis()

--- School Kit/Physics/myphys.py
+++ School Kit/Physics/myphys.py
@@ -1,4 +1,5 @@
 # Physics Formulae/Functions to be added InshaAllah
 
 # - - - - - Kinematics functions 
-V = lambda v0,a,t: v0 ± a*t # Velocity
+vf = lambda v0,a,t: v0 ± a*t # Final Velocity
+xdis = lambda v0,a,t: v0*t ± 0.5*a*t**2 # Displacement
