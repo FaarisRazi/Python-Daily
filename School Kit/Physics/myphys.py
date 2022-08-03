7e019af2049e07db0720d7eb02dc04fc7f4c44ed
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-03 22:21:33 +0400

    time functions in 1D-Kinematic form

--- School Kit/Physics/myphys.py
+++ School Kit/Physics/myphys.py
@@ -14,3 +14,7 @@ vf_0ax = lambda v0,a,x: sqrt(v0**2 + 2*a*x) # (with only v0, a, x)
 x_v0at = lambda v0, a, t: v0*t ± 0.5*a*t**2 (with only, v0, a, t)
 x_v0ft = lambda v0, vf, t: 0.5*(v0 + vf)*t (with only, v0, vf, t)
 x_v0fa = lambda v0, vf, a: 0.5*a*(vf**2 - v0**2) (with only, v0, vf, a)
+
+# Time
+t_v0fa = lambda v0, vf, a: (v - v0)/a
+t_v0fx = lambda v0, vf, x: 2*x / (v0 + v)
