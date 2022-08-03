1618da14d34441ed4e777cb9f76d19704ab970e0
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-03 18:56:04 +0400

    Update myphys.py

--- School Kit/Physics/myphys.py
+++ School Kit/Physics/myphys.py
@@ -1,5 +1,9 @@
 # Physics Formulae/Functions to be added InshaAllah
 
-# - - - - - Kinematics functions 
-vf = lambda v0,a,t: v0 ± a*t # Final Velocity
+# ------------------ Kinematics ------------------
+# Final Velocity (a = acceleration, v0 = Initial Velocity, x = displacement, t = time)
+vf_0at = lambda v0,a,t: v0 ± a*t # (with only v0, a, t)
+vf_0xt = lambda v0,x,t: 2*x/t - v0 # (with only v0, x, t)
+vf_0ax = lambda v0,a,x: sqrt(v0**2 + 2*a*x) # (with only v0, a, x)
+
 xdis = lambda v0,a,t: v0*t ± 0.5*a*t**2 # Displacement
