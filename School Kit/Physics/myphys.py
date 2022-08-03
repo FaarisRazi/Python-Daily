f7e1fa8ad34b28b811fb483a3e24c12b126e4ebb
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-03 22:28:25 +0400

    Acceleration 1D-Kinematic functions

--- School Kit/Physics/myphys.py
+++ School Kit/Physics/myphys.py
@@ -6,15 +6,20 @@ from math import sqrt
 # x = displacement, t = time)
 
 # Final Velocity
-vf_0at = lambda v0,a,t: v0 ± a*t # (with only v0, a, t)
-vf_0xt = lambda v0,x,t: 2*x/t - v0 # (with only v0, x, t)
-vf_0ax = lambda v0,a,x: sqrt(v0**2 + 2*a*x) # (with only v0, a, x)
+vf_0at = lambda v0,a,t: v0 ± a*t 
+vf_0xt = lambda v0,x,t: 2*x/t - v0
+vf_0ax = lambda v0,a,x: sqrt(v0**2 + 2*a*x)
 
 # Displacement
-x_v0at = lambda v0, a, t: v0*t ± 0.5*a*t**2 (with only, v0, a, t)
-x_v0ft = lambda v0, vf, t: 0.5*(v0 + vf)*t (with only, v0, vf, t)
-x_v0fa = lambda v0, vf, a: 0.5*a*(vf**2 - v0**2) (with only, v0, vf, a)
+x_v0at = lambda v0, a, t: v0*t ± 0.5*a*t**2
+x_v0ft = lambda v0, vf, t: 0.5*(v0 + vf)*t
+x_v0fa = lambda v0, vf, a: 0.5*(vf**2 - v0**2)/a
 
 # Time
 t_v0fa = lambda v0, vf, a: (v - v0)/a
 t_v0fx = lambda v0, vf, x: 2*x / (v0 + v)
+
+# Acceleration
+a_v0ft = lambda v0, vf, t: (v - v0)/t
+a_v0xt = lambda v0, x, t: 2*(x - v0*t)/t**2)
+a_v0fx = lambda v0, vf, x: 0.5*(vf**2 - v0**2)/x
