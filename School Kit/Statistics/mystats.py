35e54dbb53584660ef80ef21dc8cf4ea3616e85f
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-09 13:38:09 +0400

    geometric() added

--- School Kit/Statistics/mystats.py
+++ School Kit/Statistics/mystats.py
@@ -3,5 +3,9 @@
 # (Import functions from repo-directory: Python-Daily/School Kit/Maths/mymaths.py)
 from mymaths import nCr, nPr, factorial
 
-# HyperGeometric Distribution formula
-hypergeo = lambda N, Ns, n, x: ( nCr(Ns,x) * nCr(N-Ns,n-x) ) / nCr(N,n) 
+# HyperGeometric Distribution
+hypergeo = lambda N, Ns, n, x: ( nCr(Ns,x) * nCr(N-Ns,n-x) ) / nCr(N,n)
+
+# Geometric Distribution
+def geometric(p, x, sumto=False):
+  return p * (1-p)**(k-1) if not sumto else p * sum([(1-p)**(k-1) for k in range(1,x+1)])
