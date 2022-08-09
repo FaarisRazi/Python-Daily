8b9d5fc688581d3313158712e5315cdccba94a9b
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-09 14:24:06 +0400

    distrange() helper function added

--- School Kit/Statistics/mystats.py
+++ School Kit/Statistics/mystats.py
@@ -3,9 +3,28 @@
 # (Import functions from repo-directory: Python-Daily/School Kit/Maths/mymaths.py)
 from mymaths import nCr, nPr, factorial
 
+# Sum-range handler function for the distribution formulae below
+def distrange(x, sumto):
+    if sumto == True:
+        sumto = range(x+1)
+
+    elif isinstance(sumto, (range, list, tuple, set, dict)):
+        start, end = min(sumto), max(sumto)+1
+        sumto = range(start, end)
+    
+    elif sumto and not isinstance(sumto, bool):
+        raise ValueError('Invalid sumto input, please use a range or collection of numbers.')
+    
+    return sumto
+        
 # HyperGeometric Distribution
-hypergeo = lambda N, Ns, n, x: ( nCr(Ns,x) * nCr(N-Ns,n-x) ) / nCr(N,n)
+def hypergeo(N, Ns, n, x, sumto=False): 
+  formula = lambda x: ( nCr(Ns,x) * nCr(N-Ns,n-x) ) / nCr(N,n)
+  
+  return formula(x) if not sumto else sum(map(formula, distrange(sumto)))
 
 # Geometric Distribution
 def geometric(p, x, sumto=False):
-  return p * (1-p)**(k-1) if not sumto else p * sum([(1-p)**(k-1) for k in range(1,x+1)])
+  formula = lambda x: p * (1-p)**(x-1)
+  
+  return formula(x) if not sumto else sum(map(formula, distrange(sumto)))
