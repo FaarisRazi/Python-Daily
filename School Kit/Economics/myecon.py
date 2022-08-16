5dc5d947aab3ba93d0203c0faf1b70debff86e4d
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-16 15:23:07 +0400

    cpi() added

--- School Kit/Economics/myecon.py
+++ School Kit/Economics/myecon.py
@@ -2,3 +2,6 @@
 
 # Getting the Quantatity Supplied/Demanded 
 Qsd = lambda x,y,P: x + P*y # x = Units supplied/demanded at a price (y).
+
+# Consumer Price Index (ct = current cost and c0 = base cost of market basket).
+cpi = lambda ct, c0: ct/c0 * 100
