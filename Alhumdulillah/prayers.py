8b4f5e0419ec12f9a28e31cd219440d2884435d4
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-21 06:58:17 +0400

    Update prayers.py

--- Alhumdulillah/prayers.py
+++ Alhumdulillah/prayers.py
@@ -1,11 +1,8 @@
-# Prayer times from KhaleejTimes (since I'm in the UAE)
-
 import urllib.request as ur, pandas as pd, numpy as np, os
 from html_table_parser import HTMLTableParser
-from datetime import datetime
+import datetime
 
 def webtables(url, headers=''):
-    
     req = ur.Request(url=url,headers={'User-Agent': headers})
     f = ur.urlopen(req)
     xhtml = f.read().decode('utf-8')
@@ -38,42 +35,29 @@ def khaleej_prayers(name):
     return  uae_prayers_df
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~ Script ~~~~~~~~~~~~~~~~~~~~~~~~~~
-today = datetime.today()#.strftime('%d-%m-%Y')
+today = datetime.datetime.today()#.strftime('%d-%m-%Y')
 month_now = today.strftime('%B')
 
-folder = 'Islam'
-prayers_file = [i for i in os.listdir(folder) if '_prayers_' in i]
-file_comment = f'New month - {month_now} Prayer-times loaded.'
+def prayersDF():
+    folder = 'Islam'
+    prayers_file = [i for i in os.listdir(folder) if '_prayers_' in i]
+    file_comment = f'New month - {month_now} Prayer-times loaded.%s'
 
-#prayers_file = []
-if prayers_file:
-    prayers_file = prayers_file[0]
-    file_path = folder+'/'+prayers_file
+    #prayers_file = []
+    if prayers_file:
+        file_path = folder+'/'+prayers_file[0]
+
+        # If the file is NOT based on the current month's prayer times
+        if not file_path.endswith(month_now+'.npy'):
+            os.remove(file_path) # Remove the "previous month" file.
+        else:
+            print(file_comment.replace('New month','File')%'')
+            return khaleej_prayers(file_path) # Load the saved file.
     
-    # If the file is NOT based on the current month's prayer times
-    if not prayers_file.endswith(month_now+'.npy'):
-        os.remove(file_path) # Remove the "previous month" file.
-        prayers_df = khaleej_prayers(month_now) # New month's file (also saved).
-        print(file_comment + ' (and saved)')
-    else:
-        prayers_df = khaleej_prayers(file_path) # Load the saved file.
-        print(file_comment.replace('New month','File'))
-else:
-    prayers_df = khaleej_prayers(month_now)
-    print(file_comment + ' (and saved)')
+    print(file_comment%' (and saved)')
+    return khaleej_prayers(month_now)
+        
 
-# -------------- Variables to be used ---------------
-today_info = prayers_df[prayers_df[month_now] == today.day]
-month_days_left = prayers_df.loc[today_info.index[0]:]
+#prayers_df = prayersDF()
 
-days_left = month_days_left.shape[0]-1
-days_comment = '%d days left for '+month_now
-
-
-if days_left:
-    print(days_comment%days_left)
-else:
-    print('Today is the last day '+days_comment.split()[-1][:-1]+'-day')
-    
-print(month_days_left.shape[0]-1,)
-today_prayers = today_info.T[4:].rename(columns={today_info.index[0]:'times'}).rename_axis('prayers')
+#prayers_df.head()
