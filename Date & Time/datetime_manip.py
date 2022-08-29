93288d815e468ab600cf628e39a3df5774b1987a
FaarisRazi <57488592+FaarisRazi@users.noreply.github.com>
2022-08-29 13:53:36 +0400

    Update datetime_manip.py

--- Date & Time/datetime_manip.py
+++ Date & Time/datetime_manip.py
@@ -1,11 +1,16 @@
-# Using Prayers.ipynb example, to be continued InshaAllah.
-def time_diff(x, colname):
+# See example-file from >  Alhumdulillah/Prayers.ipynb
+def time_diff(time_col, strf, until_next = True, dur=True):
+    # strf = '%I:%M %p' (like "2:30 PM").
+    # "until_next" format to show "duration" of one time-index until the next (per row).
     
-    df[colname] = df.diff().fillna(pd.Timedelta(seconds=0))
-    #Or: diff().dt.seconds.fillna(0).apply(duration, 0) # duration() from my repo: Python-Daily/Date & Time/duration
-
-#     today_prayers.loc[0, colname] = prayers_astime.iloc[::-5].diff()[-1]
-#     today_prayers['until_next'] = today_prayers.until_next.apply(lambda x: str(x).split()[-1])
-#     today_prayers['until_next'] = np.append(today_prayers.until_next[1:].values, 
-#                                             today_prayers.loc['Fajr', 'until_next'])
-#     today_prayers
+    time_df = pd.to_datetime(time_col, format=strf)
+    time_df = time_df.diff().fillna(pd.Timedelta(seconds=0))
+    
+    if until_next:
+        time_f = time_df.iloc[::-time_df.shape[0] + 1].diff()[-1] # Time difference between the last and first rows.
+        time_df = np.append(time_df[1:].values, time_f) # Re-adjust DF to show durations in "until next" row format.
+    
+    if dur:
+        time_df = time_df.apply(lambda x: str(x).split()[-1])
+        
+    return time_df
